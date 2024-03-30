f17b86a35ff6ff8312d27a1b656a5a859ae02365
Dadi Farisa <119612727+Dodolspartan@users.noreply.github.com>
2024-03-30 23:37:19 +0700

    update v.10

--- js/app.js
+++ js/app.js
@@ -34,3 +34,21 @@ function hero() {
 }
 
 window.addEventListener("scroll", hero);
+
+//Owl Coursel
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
