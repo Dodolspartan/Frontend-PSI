a024d0f3b3a7deab1fbeaa9b4ab44de9fc75119d
Dadi Farisa <119612727+Dodolspartan@users.noreply.github.com>
2024-04-03 05:55:28 +0700

    update detail pekerjaan

--- js/app.js
+++ js/app.js
@@ -56,3 +56,16 @@ const swiper = new Swiper(".MySwiper", {
     el: ".swiper-scrollbar",
   },
 });
+// Clipboard
+
+function myFunction() {
+  // Get the text field
+  var copyText = document.getElementById("myInput");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+}
