5ed964acdfb434a65bec1a7adf063ced84cb9039
Dadi Farisa <119612727+Dodolspartan@users.noreply.github.com>
2024-04-24 11:31:34 +0700

    update drs.

--- js/validate.js
+++ js/validate.js
@@ -1,5 +1,11 @@
-// Step 1 Validate
-
+// Step 1 Validate NIK
+function checkInput(e, chars, field) {
+  if (chars.indexOf(e.key) != -1) {
+    return true;
+  } else {
+    return false;
+  }
+}
 // Step 2 Validate
 // Step 3 Validate
 // Step 4 Validate
